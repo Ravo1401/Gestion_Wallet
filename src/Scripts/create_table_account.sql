7b3e83feea6872b4313428246fde3fa185158bbe
Fifaliana <hei.fifaliana@gmail.com>
2023-12-04 09:20:33 +0300

    feat: new scripts for creating table

--- src/Scripts/create_table_account.sql
+++ src/Scripts/create_table_account.sql
@@ -3,6 +3,12 @@
 CREATE TABLE IF NOT EXISTS Account (
     accountId VARCHAR(50) PRIMARY KEY,
     balance DOUBLE PRECISION NOT NULL,
+    accountName VARCHAR(50) NOT NULL ,
     currencyCode VARCHAR(3) REFERENCES Currency(currencyCode),
     CONSTRAINT positiveBalance CHECK (balance >= 0)
 );
+
+INSERT INTO Account (accountId ,balance ,accountName , currencyCode)
+VALUES
+('QSDF12345' , 45000.0 ,'Depos', 'EUR'),
+('12345dsdf',  60000.0 ,'nice', 'AR');
--- src/Scripts/create_table_currency.sql
+++ src/Scripts/create_table_currency.sql
@@ -1,6 +1,11 @@
 -- Script pour créer la table Currency
 CREATE TABLE IF NOT EXISTS Currency (
-    currencyCode VARCHAR(3) PRIMARY KEY,
+    id SERIAL PRIMARY KEY ,
+    currencyCode VARCHAR(3) NOT NULL,
     currencyName VARCHAR(50) NOT NULL
 );
 
+ insert into Currency (currencyCode , currencyName)
+ VALUES 
+ ('AR' , 'ARIARY'),
+ ('EUR' , 'EURO');
