b5df3fb75b713c1a56463f7ef2d3f1bb1eeca50c
Fifaliana <hei.fifaliana@gmail.com>
2023-12-04 09:20:04 +0300

    feat: models of currency

--- src/Models/Currency.java
+++ src/Models/Currency.java
@@ -3,4 +3,33 @@ package Models;
 public class Currency {
     private String currencyCode;
     private String currencyName;
+
+    public Currency(String currencyCode, String currencyName) {
+        this.currencyCode = currencyCode;
+        this.currencyName = currencyName;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getCurrencyName() {
+        return currencyName;
+    }
+
+    public void setCurrencyName(String currencyName) {
+        this.currencyName = currencyName;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "currencyCode='" + currencyCode + '\'' +
+                ", currencyName='" + currencyName + '\'' +
+                '}';
+    }
 }
