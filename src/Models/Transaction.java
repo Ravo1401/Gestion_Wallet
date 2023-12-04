cf1e38505e607fe4f9101e61bef9c67560e7a9ba
Fifaliana <hei.fifaliana@gmail.com>
2023-12-04 18:42:16 +0300

    feat: transaction for adding a account id

--- src/Models/Transaction.java
+++ src/Models/Transaction.java
@@ -3,9 +3,79 @@ package Models;
 import java.sql.Date;
 
 public class Transaction {
-    private String transactionId;
+    private int transactionId;
     private Date date;
     private String description;
     private double amount;
     private TransactionType type;
+    private String accountId ;
+
+    public Transaction(int transactionId, Date date, String description, double amount, TransactionType type, String accountId) {
+        this.transactionId = transactionId;
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+        this.type = type;
+        this.accountId = accountId;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", date=" + date +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", type=" + type +
+                ", accountId='" + accountId + '\'' +
+                '}';
+    }
 }
