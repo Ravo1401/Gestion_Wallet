b5a18efe99d4eea289b4f0fa985ab86922721340
Fifaliana <hei.fifaliana@gmail.com>
2023-12-04 09:18:41 +0300

    feat: Account attribut

--- src/Models/Account.java
+++ src/Models/Account.java
@@ -1,8 +1,69 @@
 package Models;
 
+import java.util.List;
+
 public class Account {
     private String accountId ;
+    private String accountName ;
     private double balance;
-    private Currency currencyCode;
+    private String currencyCode;
+    private List<Transaction> transactions ;
 
+    public Account(String accountId, String accountName, double balance, String currencyCode) {
+        this.accountId = accountId;
+        this.accountName = accountName;
+        this.balance = balance;
+        this.currencyCode = currencyCode;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId='" + accountId + '\'' +
+                ", accountName='" + accountName + '\'' +
+                ", balance=" + balance +
+                ", currencyCode='" + currencyCode + '\'' +
+                ", transactions=" + transactions +
+                '}';
+    }
 }
